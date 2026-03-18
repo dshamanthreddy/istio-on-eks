41e0a488ea764aedc7009dd0b29725e2034dc62c
dshamanthreddy <devagarishamanth.com>
2026-03-18 18:07:50 -0400

    feat: added TF automode for istio ambient

--- terraform-blueprint/ambient/main.tf
+++ terraform-blueprint/ambient/main.tf
@@ -37,8 +37,10 @@ locals {
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
+  eks_cluster_version = "1.34"
+
   istio_chart_url     = "https://istio-release.storage.googleapis.com/charts"
-  istio_chart_version = "1.22.0"
+  istio_chart_version = "1.28.1"
 
   tags = {
     Blueprint  = local.name
@@ -52,34 +54,25 @@ locals {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.11"
+  version = "~> 21.0"
 
-  cluster_name                   = local.name
-  cluster_version                = "1.30"
-  cluster_endpoint_public_access = true
+  name                   = local.name
+  kubernetes_version     = local.eks_cluster_version
+  endpoint_public_access = true
 
   # Give the Terraform identity admin access to the cluster
   # which will allow resources to be deployed into the cluster
   enable_cluster_creator_admin_permissions = true
 
-  cluster_addons = {
-    coredns    = {}
-    kube-proxy = {}
-    vpc-cni    = {}
+  # enable_irsa = false
+
+  compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose","system"]
   }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-
-  eks_managed_node_groups = {
-    initial = {
-      instance_types = ["m5.large"]
-
-      min_size     = 1
-      max_size     = 5
-      desired_size = 2
-    }
-  }
 
   #  EKS K8s API cluster needs to be able to talk with the EKS worker nodes with port 15017/TCP and 15012/TCP which is used by Istio
   #  Istio in order to create sidecar needs to be able to communicate with webhook and for that network passage to EKS is needed.
@@ -113,19 +106,25 @@ resource "kubernetes_namespace_v1" "istio_system" {
   metadata {
     name = "istio-system"
   }
+
+  depends_on = [ module.eks ]
+}
+
+resource "kubernetes_namespace_v1" "istio_ingress" {
+  metadata {
+    name = "istio-ingress"
+  }
+  depends_on = [ module.eks ]
 }
 
 module "eks_blueprints_addons" {
   source  = "aws-ia/eks-blueprints-addons/aws"
-  version = "~> 1.16"
+  version = "~> 1.23"
 
   cluster_name      = module.eks.cluster_name
   cluster_endpoint  = module.eks.cluster_endpoint
   cluster_version   = module.eks.cluster_version
   oidc_provider_arn = module.eks.oidc_provider_arn
-
-  # This is required to expose Istio Ingress Gateway
-  enable_aws_load_balancer_controller = true
 
   helm_releases = {
     istio-base = {
@@ -168,6 +167,21 @@ module "eks_blueprints_addons" {
           value = "ambient"
         }
       ]
+
+      values = [
+        yamlencode({
+          nodeSelector = {
+            "karpenter.sh/nodepool" = "system"
+          }
+          tolerations = [
+            {
+              key = "CriticalAddonsOnly"
+              operator = "Exists"
+              effect = "NoSchedule"
+            }
+          ]
+        })
+      ]
     }
 
     ztunnel = {
@@ -177,36 +191,11 @@ module "eks_blueprints_addons" {
       name          = "ztunnel"
       namespace     = kubernetes_namespace_v1.istio_system.metadata[0].name
     }
-
-    istio-ingress = {
-      chart            = "gateway"
-      chart_version    = local.istio_chart_version
-      repository       = local.istio_chart_url
-      name             = "istio-ingress"
-      namespace        = "istio-ingress" # per https://github.com/istio/istio/blob/master/manifests/charts/gateways/istio-ingress/values.yaml#L2
-      create_namespace = true
-
-      values = [
-        yamlencode(
-          {
-            labels = {
-              istio = "ingressgateway"
-            }
-            service = {
-              annotations = {
-                "service.beta.kubernetes.io/aws-load-balancer-type"            = "external"
-                "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
-                "service.beta.kubernetes.io/aws-load-balancer-scheme"          = "internet-facing"
-                "service.beta.kubernetes.io/aws-load-balancer-attributes"      = "load_balancing.cross_zone.enabled=true"
-              }
-            }
-          }
-        )
-      ]
-    }
   }
 
   tags = local.tags
+
+  depends_on = [ kubernetes_namespace_v1.istio_system, kubernetes_namespace_v1.istio_ingress ]
 }
 
 ################################################################################
@@ -215,7 +204,7 @@ module "eks_blueprints_addons" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
+  version = "~> 6.0"
 
   name = local.name
   cidr = local.vpc_cidr
--- terraform-blueprint/ambient/versions.tf
+++ terraform-blueprint/ambient/versions.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.34"
+      version = ">= 6.27"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.9"
+      version = ">= 2.17"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.20"
+      version = ">= 3.0.1"
     }
   }
 
